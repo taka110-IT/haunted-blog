84e7b56aa4c750d43286dde3811f7edf2b4c8180
taka110-IT <onyourmark_aska@yahoo.co.jp>
2022-09-26 01:47:58 +0900

    脆弱性を修正した

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,8 @@
 class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
-  before_action :set_blog, only: %i[show edit update destroy]
+  before_action :set_blog, only: %i[show]
+  before_action :set_current_user_blog, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -44,10 +45,27 @@ class BlogsController < ApplicationController
   private
 
   def set_blog
-    @blog = Blog.find(params[:id])
+    @blog = if Blog.find(params[:id]).secret == false
+              Blog.find(params[:id])
+            elsif user_signed_in?
+              current_user.blogs.find(params[:id])
+            else
+              raise ActiveRecord::RecordNotFound
+            end
+  end
+
+  def set_current_user_blog
+    if current_user.blogs.find(params[:id]).present?
+      @blog = current_user.blogs.find(params[:id])
+    end
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    permit_items = [:title, :content, :secret]
+    if current_user.premium?
+      params.require(:blog).permit(permit_items, :random_eyecatch)
+    else
+      params.require(:blog).permit(permit_items)
+    end
   end
 end
